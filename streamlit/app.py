235ccfa4e43d045eebfd62c55f3066b87230e306
MRESENDESILVA <mateus.rsilva@sptech.school>
2025-07-26 16:49:44 -0300

    refact: test only with streamlit

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,6 +1,10 @@
 import streamlit as st
-import requests
+import pickle
+import joblib
+import pandas as pd
 import logging
+from functions import MinMax, OneHotEncodeNames, OrdinalEncodeNames, BinarioTransformer, DropFeatures, Oversample
+
 
 
 st.set_page_config(page_title="Formulário de Previsão", layout="centered")
@@ -45,15 +49,79 @@ if st.button("🔮 Prever"):
         "mtrans": mtrans
     }
 
-    try:
-        response = requests.post("http://api:5000/predict", json=input_data)
-        
+    
+    st.success(f"Resultado da Previsão: **{result['data']['prediction']}**")
 
-        if response.status_code == 200:
-            result = response.json()
-            st.success(f"Resultado da Previsão: **{result['data']['prediction']}**")
-        else:
-            st.error(f"Erro na predição: {response.status_code} - {response.text}")
 
-    except requests.exceptions.ConnectionError:
-        st.error("❌ Não foi possível conectar à API. Verifique se o servidor está online.")
+def predict(data):
+
+    pipeline = joblib.load('pipeline.pkl')
+    modelo = joblib.load('modelo.pkl')
+    
+    #Classe para validar as entradas
+    class InputData(BaseModel):
+        gender: str
+        age: int
+        height: float
+        weight: float
+        family_history: str
+        favc: str
+        fcvc: str
+        ncp: str
+        caec: str
+        smoke: str
+        ch20: str
+        scc: str
+        faf: str
+        calc: str
+        mtrans: str
+
+    obesity_classes = {
+        0: 'Abaixo_do_Peso',
+        1: 'Peso_Normal',
+        2: 'Sobrepeso_Level_I',
+        3: 'Sobrepeso_Level_II',
+        4: 'Obesidade_Tipo_I',
+        5: 'Obesidade_Tipo_II',
+        6: 'Obesidade_Tipo_III'
+    }
+
+
+    input_data = InputData(data)
+
+    dados = {
+        'Gender': [input_data.gender],
+        'Age': [input_data.age],
+        'Height': [input_data.height],
+        'Weight' : [input_data.weight],
+        'family_history': [input_data.family_history],
+        'FAVC': [input_data.favc],
+        'FCVC': [input_data.fcvc],
+        'NCP': [input_data.ncp],
+        'CAEC': [input_data.caec],
+        'SMOKE': [input_data.smoke],
+        'CH2O': [input_data.ch20],
+        'SCC': [input_data.scc],
+        'FAF': [input_data.faf],
+        'CALC': [input_data.calc],
+        'MTRANS': [input_data.mtrans],
+        'Obesity': ['Peso_Normal']
+    }
+
+
+    df_amostra = pd.DataFrame(dados)
+    x_novo_dado = pipeline.transform(df_amostra)
+    x_transformado = x_novo_dado.drop(columns=['Obesity']).head()
+    y_predito = modelo.predict(x_transformado)
+
+
+    if y_predito == 0:
+        return 'Risco de ficar abaixo do peso'
+    elif y_predito in [1,2,3]:
+        return 'Sem riscos'
+    elif y_predito in [4,5,6]:
+        return 'Risco de desenvolver obesidade se continuar com esses hábitos'
+    else:
+        return "error"
+
+
